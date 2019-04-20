5514a2818b8203174fc99db5db1bccef31824a82
David Weid <david.weid.2@gmail.com>
2019-04-20 17:38:29 -0400

    Updating basic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import { Button } from "reactstrap";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Main from "./pages/Main";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>Content</div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Main} />
+        </Wrapper>
       </Router>
     );
   }
