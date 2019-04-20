5cfd9eea493d390dd1aaafd1eba93927c37a65bb
Mvcampbell3 <mvcampbell3@gmail.com>
2019-04-20 17:35:48 -0400

    example API call with example api component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
+import API from "./utils/API";
+
+// Components
+import ExampleCall from "./components/ExampleCall"
+
 class App extends Component {
+  state = {
+    photos: []
+  }
+
+  componentDidMount() {
+    // Just for showing some of the photos we expect to get back
+    // Will not use for real version
+    this.getExamplePhotos()
+  }
+
+  getExamplePhotos() {
+    API.exampleAPI()
+    .then(res => this.setState({photos: res.data.photos}))
+    .catch(err => console.log(err));
+  }
+
+
   render() {
     return (
       <Router>
-        <div>Content</div>
+        <ExampleCall photos={this.state.photos} />
       </Router>
     );
   }
