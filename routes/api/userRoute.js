fb3af9943764b0cbcf701da5efa077db5d46fe36
Mvcampbell3 <mvcampbell3@gmail.com>
2019-04-25 21:25:19 -0400

    expanded user model and api routes

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
     .catch(err => res.json(err));
 })
 
-router.post("/", (req, res) => {
+router.post("/signup", (req, res) => {
   const newUser = new User(req.body);
 
   newUser.save()
@@ -16,16 +16,33 @@ router.post("/", (req, res) => {
 })
 
 router.post("/login", (req, res) => {
-  User.findOne({ username: req.body.username }, function(err, user) {
-    if (err) throw err;
-    
-    user.comparePassword(req.body.password, function(err, resp){
-      if (err) res.json(err);
-      console.log(resp)
-      res.json(resp);
+  User.findOne({ username: req.body.username})
+    .then(user => {
+      user.comparePassword(req.body.password, function(err, resp) {
+        if (err) res.json(err);
+        console.log(resp)
+        res.json(resp);
+      })
     })
+    .catch(err => {
+      res.status(404).json({error:"There is no user by that username"})
+    })
+})
 
-  })
+router.delete("/:id", (req,res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(deleted => res.json({deleted: deleted}))
+    .catch(err => res.status(404).json({err: err}))
+})
+
+router.put("/:id", (req,res) => {
+  if (req.body.postIDs) {
+    console.log("they made it");
+    User.update({username: req.params.id})
+  }
+  User.findByIdAndUpdate(req.params.id, req.body.update)
+    .then(result =>  res.json(result))
+    .catch(err => res.json(err))
 })
 
 module.exports = router;
