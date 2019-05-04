0aba8d804bdaf6aa7cb87818b7dca509388e262f
David Weid <david.weid.2@gmail.com>
2019-05-03 22:20:59 -0400

    Update to visuals and mobile responsiveness.

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -4,7 +4,11 @@ import "./banner.css";
 const Banner = props => (
   <div
     className="banner-image"
-    style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${props.backgroundImage})` }}
+    style={{
+      backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${
+        props.backgroundImage
+      })`
+    }}
   >
     <div className="banner-text">{props.children}</div>
   </div>
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./style.css";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
 class Login extends Component {
   state = {
@@ -8,61 +8,74 @@ class Login extends Component {
     show: false,
     username: "",
     email: "",
-    password: "",
-  }
+    password: ""
+  };
 
-  handleInputUpdate = (e) => {
+  handleInputUpdate = e => {
     const keyData = e.target.name;
     const valueData = e.target.value;
-    this.setState({ [keyData]: valueData })
-  }
+    this.setState({ [keyData]: valueData });
+  };
 
-  showLogin = (e) => {
+  showLogin = e => {
     e.preventDefault();
     if (!this.props.user) {
-      this.setState({ show: !this.state.show })
+      this.setState({ show: !this.state.show });
     }
-  }
+  };
 
-  switchForm = (e) => {
+  switchForm = e => {
     e.preventDefault();
-    this.setState({ login: !this.state.login })
-  }
+    this.setState({ login: !this.state.login });
+  };
 
   // Might merge login and signup into same function based on if email...
-  // it already works though so 
+  // it already works though so
 
-  login = (e) => {
+  login = e => {
     e.preventDefault();
     API.userLogin(this.state.email, this.state.password)
       .then(res => {
         console.log(res.data.user);
-        this.props.changeUserState(res.data.user)
-        this.setState({ show: false })
+        this.props.changeUserState(res.data.user);
+        this.setState({ show: false });
       })
       .catch(err => {
-        console.log("this is from login component")
+        console.log("this is from login component");
         alert("You have entered incorrect password or email");
-        this.setState({password: ""})
-        console.log(err)
-
+        this.setState({ password: "" });
+        console.log(err);
       });
-  }
+  };
 
-  signup = (e) => {
+  signup = e => {
     e.preventDefault();
     API.userSignup(this.state.username, this.state.password, this.state.email)
       .then(res => console.log(res))
       .catch(err => console.log(err));
-  }
+  };
 
   render() {
     return (
       <div className="heightUser">
-        <div className="banLink" onClick={(e) => this.showLogin(e)}>{this.props.user ? "Logout" : "Login"}</div>
-        {!this.props.user ? (
-          <div id="box" className="loginArea" style={{ display: this.state.show ? "block" : "none" }}>
+        {this.props.user ? (
+          <div className="banLink" onClick={e => this.showLogin(e)}>
+            <span>
+              <i className="fas fa-user-astronaut fa-2x" />
+            </span>
+          </div>
+        ) : (
+          <div className="banLink" onClick={e => this.showLogin(e)}>
+            Login
+          </div>
+        )}
 
+        {!this.props.user ? (
+          <div
+            id="box"
+            className="loginArea"
+            style={{ display: this.state.show ? "block" : "none" }}
+          >
             {this.state.login ? (
               <div id="loginForm" className="login form-padding">
                 <form>
@@ -72,7 +85,7 @@ class Login extends Component {
                       type="text"
                       name="email"
                       value={this.state.email}
-                      onChange={(e) => this.handleInputUpdate(e)}
+                      onChange={e => this.handleInputUpdate(e)}
                     />
                   </div>
                   <div className="loginFormGroup">
@@ -81,82 +94,80 @@ class Login extends Component {
                       type="password"
                       name="password"
                       value={this.state.password}
-                      onChange={(e) => this.handleInputUpdate(e)}
+                      onChange={e => this.handleInputUpdate(e)}
                     />
                   </div>
                   <div className="formButtons">
                     <button
                       type="submit"
                       className="formSubBtn"
-                      onClick={(e) => this.login(e)}
-                    >Submit
+                      onClick={e => this.login(e)}
+                    >
+                      Submit
                     </button>
                     <button
-                      onClick={(e) => this.switchForm(e)}
+                      onClick={e => this.switchForm(e)}
                       className="formSubBtn"
-                    >Sign Up
+                    >
+                      Sign Up
                     </button>
                   </div>
-
                 </form>
               </div>
-
             ) : (
-
-                <div id="signupForm" className="signup form-padding">
-                  <form>
-                    <div className="loginFormGroup">
-                      <label>Email:</label>
-                      <input
-                        type="email"
-                        name="email"
-                        value={this.state.email}
-                        onChange={(e) => this.handleInputUpdate(e)}
-                      />
-                    </div>
-                    <div className="loginFormGroup">
-                      <label>Username:</label>
-                      <input
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={(e) => this.handleInputUpdate(e)}
-                      />
-                    </div>
-                    <div className="loginFormGroup">
-                      <label>Password:</label>
-                      <input
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={(e) => this.handleInputUpdate(e)}
-                      />
-                    </div>
-                    <div className="formButtons">
-                      <button
-                        type="submit"
-                        className="formSubBtn"
-                        onClick={(e) => this.signup(e)}
-                      >Submit
-                      </button>
-                      <button
-                        onClick={(e) => this.switchForm(e)}
-                        className="formSubBtn"
-                      >Login
-                      </button>
-                    </div>
-                  </form>
-                </div>)}
-
-
+              <div id="signupForm" className="signup form-padding">
+                <form>
+                  <div className="loginFormGroup">
+                    <label>Email:</label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={e => this.handleInputUpdate(e)}
+                    />
+                  </div>
+                  <div className="loginFormGroup">
+                    <label>Username:</label>
+                    <input
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      onChange={e => this.handleInputUpdate(e)}
+                    />
+                  </div>
+                  <div className="loginFormGroup">
+                    <label>Password:</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={e => this.handleInputUpdate(e)}
+                    />
+                  </div>
+                  <div className="formButtons">
+                    <button
+                      type="submit"
+                      className="formSubBtn"
+                      onClick={e => this.signup(e)}
+                    >
+                      Submit
+                    </button>
+                    <button
+                      onClick={e => this.switchForm(e)}
+                      className="formSubBtn"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
           </div>
         ) : (
-            <div></div>
-          )}
-
+          <div />
+        )}
       </div>
-
-    )
+    );
   }
 }
 
