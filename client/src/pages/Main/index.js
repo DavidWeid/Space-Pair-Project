2c485fa42505f4661d0143d47dc9bef7de6b17c1
David Weid <david.weid.2@gmail.com>
2019-05-06 10:47:31 -0400

    Working on the landing page

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Navbar from "../../components/Navbar";
+import "./landing.css";
 
 class Main extends Component {
   state = {
@@ -10,8 +10,21 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar brand="Welcome" home="/" pageOne="Data" pageTwo="Forum" pageThree="Profile" />
+      <div className="bg">
+        <div className="bg-left">
+          <div className="action-div">
+            <item className="action-item">
+              <span className="action-wrd">Discover</span> Mars
+            </item>
+            <item className="action-item">
+              <span className="action-wrd">Learn</span> the Rovers
+            </item>
+            <item className="action-item">
+              <span className="action-wrd">Join</span> a Community
+            </item>
+          </div>
+        </div>
+        <div className="bg-right">Space</div>
       </div>
     );
   }
