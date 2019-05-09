d592122e1496c92e4f49d8c8f2325e36206ba6da
David Weid <david.weid.2@gmail.com>
2019-05-09 18:53:38 -0400

    Inactivated console logs for forum page. Working on grabbing data for Profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -10,17 +10,47 @@ const urlPic =
   "https://images.pexels.com/photos/87080/space-shuttle-start-discovery-spaceport-87080.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
 
 class Profile extends Component {
+
+  state = {
+    hopeful: true,
+    userInfo: {},
+    postsCreated: [],
+    postsSaved: [],
+    postsLiked: []
+  }
+
   componentWillMount() {
     if (this.props.user) {
-      API.grabUserInfo()
-        .then(result => console.log(result))
-        .then(
-          API.getAllSavedPosts()
-            .then(result => console.log(result))
-            .catch(err => console.log(err))
-        );
+      this.getUserInfo();
+      this.getPostsUserCreated();
+      this.getPostsUserSaved();
+      this.getPostsUserLiked();
     }
   }
+
+  getUserInfo = () => {
+    API.getUserInfo()
+      .then(res => console.log("User Info\n", res.data))
+      .catch(err => console.log(err));
+  };
+
+  getPostsUserCreated = () => {
+    API.getPostsUserCreated()
+      .then(res => console.log("Posts Created by User\n", res.data))
+      .catch(err => console.log(err));
+  };
+
+  getPostsUserSaved = () => {
+    API.getPostsUserSaved()
+      .then(res => console.log("Posts Saved by User\n", res.data))
+      .catch(err => console.log(err));
+  };
+
+  getPostsUserLiked = () => {
+    API.getPostsUserLiked()
+      .then(res => console.log("Posts Liked by User\n", res.data))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
