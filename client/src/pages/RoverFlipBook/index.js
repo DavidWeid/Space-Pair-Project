ee3a941d14fda91bdc5895d1ef65304a4949e9e2
Mvcampbell3 <mvcampbell3@gmail.com>
2019-05-14 17:45:59 -0400

    getting the flip book closer

--- client/src/pages/RoverFlipBook/index.js
+++ client/src/pages/RoverFlipBook/index.js
@@ -5,40 +5,92 @@ import API from "../../utils/API";
 class RoverFlipBook extends Component {
   state = {
     working: true,
-    photos: []
+    photos: [
+
+    ],
   }
 
-  componentWillMount() {
-    this.testPictures();
+  componentDidMount() {
+    this.testPictures()
   }
 
   testPictures = () => {
-    API.roverSolCameraPictures("opportunity", "133", "FHAZ")
-      .then(result => {
-        console.log(result.data.photos);
-        const setter = result.data.photos;
-        this.setState({ photos: setter })
+    API.roverSolCameraPictures("opportunity", "133", "navcam")
+    .then(result => {
+      const term = result.data.photos;
+      console.log(term);
+      console.log(this);
+      term.forEach(one => {
+        this.setState(prevState => ({
+          photos: [...prevState.photos, one]
+        }))
       })
-      .catch(err => console.log(err));
+
+    })
+    .catch(err => console.log(err));
   }
 
   testing = (e) => {
     console.log(this.state.photos)
   }
 
-  loadImages = () => {
-    
+  rollImages = () => {
+    const pics = [].slice.call(document.querySelectorAll(".newPic"));
+    console.log(pics);
+    let pos = -1;
+    const timer = setInterval(function() {
+      if (pos >= pics.length - 1) {
+        clearInterval(timer);
+        console.log("done");
+      } else {
+        pos++;
+        pics.forEach(one => one.style.zIndex = "1");
+        console.log(pics[pos]);
+        pics[pos].style.zIndex = "25";
+      }
+    }, 175)
   }
 
   render() {
-
+    let loaded = 0;
+    let pictures = [];
 
     return (
-      <div>
-        <button onClick={this.loadImages}>Load</button>
-        {this.state.photos.length > 0 ? <div>
-          <canvas id="canvas" />
-        </div> : <h2>Not here</h2>}
+      <div className="centering">
+
+        <button onClick={this.rollImages}>Roll</button>
+        {this.state.photos.length > 0 ? <div id="outputArea">
+
+          <h1>Loading</h1>
+          {this.state.photos.forEach((photo, i) => {
+            const img = new Image();
+            img.alt = "new picture"
+            img.className = "newPic"
+            img.onload = () => {
+              loaded++;
+              if (loaded === this.state.photos.length) {
+                console.log("all loaded");
+                console.log(pictures);
+                // pictures.forEach(one => outputArea.append(one));
+                const outputArea = document.getElementById("outputArea");
+                console.log(outputArea);
+                outputArea.innerHTML = "";
+                pictures.forEach(one => outputArea.append(one))
+                const newDiv = document.createElement("div");
+                newDiv.className = "blocker";
+                outputArea.append(newDiv)
+              } else {
+                console.log("loaded " + i);
+                pictures.push(img);
+                
+              }
+            }
+            // img.src = this.state.photos[i];
+            img.src = photo.img_src;
+            // return img;
+          }
+          )}
+        </div> : <h2>No Pics</h2>}
       </div>
     )
   }
