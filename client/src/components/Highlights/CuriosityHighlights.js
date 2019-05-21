7ad2a694ea4ce3f6bf255b48557b33172cf85016
Mvcampbell3 <mvcampbell3@gmail.com>
2019-05-20 22:14:30 -0400

    added highlights for oppo and curiosity, fixed saved articles bug

--- client/src/components/Highlights/CuriosityHighlights.js
+++ client/src/components/Highlights/CuriosityHighlights.js
@@ -4,7 +4,47 @@ import Highlighter from "./Hightlighter";
 class CuriosityHighlights extends Component {
   state = {
     highlightArr: [
-      
+      {
+        sol: "2",
+        description: "Curiosity Landing Site, Gale Crater",
+        camera: "NAVCAM",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00002/opgs/edr/ncam/NLA_397681339EDR_F0020000AUT_04096M_.JPG"
+      },
+
+      {
+        sol: '182',
+        description: "Digging Holes",
+        camera: "FHAZ",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/00182/opgs/edr/fcam/FLA_413639557EDR_F0060000FHAZ00316M_.JPG"
+      },
+
+      {
+        sol: "389",
+        description: "Phobos Solar Eclipse",
+        camera: "MAST",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/msss/00369/mcam/0369MR1502000769L1_DXXX.jpg"
+      },
+
+      {
+        sol: "640",
+        description: "Iron Meteorite",
+        camera: "MAST",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/msss/00640/mcam/0640MR0027180200401839E02_DXXX.jpg"
+      },
+
+      {
+        sol: "1099",
+        description: "Mount Sharp",
+        camera: "MAST",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/msss/01099/mcam/1099MR0048620000600827C00_DXXX.jpg"
+      },
+
+      {
+        sol: "2125",
+        description: "Planet-wide Dust Storm",
+        camera: "NAVCAM",
+        url: "https://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/02125/opgs/edr/ncam/NRB_586124435EDR_M0720386NCAM00580M_.JPG"
+      }
     ]
 
   }
--- client/src/components/Highlights/OpportunityHighlights.js
+++ client/src/components/Highlights/OpportunityHighlights.js
@@ -4,7 +4,68 @@ import Highlighter from "./Hightlighter";
 class OpportunityHighlights extends Component {
   state = {
     highlightArr: [
-     
+      {
+        sol: "1",
+        description:"Hole in One, Bedrock",
+        camera: "NAVCAM",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/n/001/1N128285132EDN0000P1500R0M1-BR.JPG"
+      },
+
+      {
+        sol: "14",
+        description: "Hematite 'Blueberries'",
+        camera: "MINI-TES",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/m/014/1M129430201EFF0300P2932M1M1-BR.JPG"
+      },
+
+      {
+        sol: "48",
+        description: "Travel distance to find evidence of water",
+        camera: "RHAZ",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/r/048/1R132452977EFF0600P1312L0M1-BR.JPG"
+      },
+
+      {
+        sol: "133",
+        description: "Endurance Crater",
+        camera: "NAVCAM",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/n/133/1N139999062EFF3140P1998L0M1-BR.JPG"
+      },
+
+      {
+        sol: "180",
+        description: "Self Portrait",
+        camera: "FHAZ",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/f/180/1F144177858EFF3352P1214R0M1-BR.JPG"
+      },
+
+      {
+        sol: "479",
+        description: "Getting Un-Stuck",
+        camera: "RHAZ",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/r/479/1R170716732EFF55PSP1314R0M1-BR.JPG"
+      },
+
+      {
+        sol: "709",
+        description: "Phobos Solar Eclipse",
+        camera: "PANCAM",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/p/709/1P191143171ESF64KSP2669R8M1-BR.JPG"
+      },
+
+      {
+        sol: "952",
+        description: "Victoria Crater",
+        camera: "NAVCAM",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/n/952/1N212701129EFF76EVP0657R0M1-BR.JPG"
+      },
+
+      {
+        sol:"5111",
+        description: "It's dark outside and my batteries are low",
+        camera: "PANCAM",
+        url: "https://mars.nasa.gov/mer/gallery/all/1/p/5111/1P581919922EFFD2FCP2682L8M1-BR.JPG"
+      }
     ]
 
   }
